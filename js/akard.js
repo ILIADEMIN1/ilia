b7ef26e399bcea1db5307032afd432234e6ac7de
ILIADEMIN1 <98646439+ILIADEMIN1@users.noreply.github.com>
2022-01-29 20:15:12 +0300

    Add files via upload

--- js/akard.js
+++ js/akard.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     // const buttons = document.querySelectorAll(".accordion-header");
+    // создается константа ищется обьект с классом .accordion
     const accordions = document.querySelectorAll(".accordion");
-
+    // выполняется функция форич
     accordions.forEach((accordion) => {
         const items = accordion.querySelectorAll(".accordion-item");
         items.forEach((item) => {
             const button = item.querySelector(".accordion-header");
             const content = item.querySelector(".accordion-content");
+            // открытие и закрытие по клику
             button.addEventListener("click", () => {
                 item.querySelector(".accordion__button").classList.toggle("active");
                 content.classList.toggle("active");
